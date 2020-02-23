7d809a425677dfc49bb652788b002357f0d66b3a
SamAlJuMa <samuelaljuma@gmail.com>
2020-02-23 02:05:25 -0600

    Clase Transicion/Arreglo de errores

--- practica1Py/AFN/Automata.py
+++ practica1Py/AFN/Automata.py
@@ -1,88 +1,93 @@
 from Estados import *
+from Transiciones import *
 
 class Automata(object):
 
-    def __init__(self):
-        self._edoInit=null
-        self._alfabeto=set()
-        self._edosAceptacion=set()
-        self._edosAFN=set()
-
-    def __init__(self,simbolo):
-        self._edoInit=Estados()
-        self._alfabeto=set(simbolo)
-        self._edoInit.setName("0")
-        ef=Estados()
-        ef.setAceptacion(True)
-        ef.setName("f")
-        transicion="( "+simbolo+","+ef.getName()+" )"
-        print("Transicion generada: "+transicion)
-        self._edoInit.getTransiciones().append(transicion)
-        self._edosAFN=list()
-        self._edosAFN.append(self._edoInit)
-        self._edosAFN.append(ef)
-        self._edosAceptacion=list()
-        self._edosAceptacion.append(ef)
-
+    def __init__(self, simbolo=None):
+        if simbolo is not None:
+            self._edoInit = Estados()
+            self._alfabeto = set(simbolo)
+            self._edoInit.setName("0")
+            ef = Estados()
+            ef.setAceptacion(True)
+            ef.setName("f")
+            #transicion = "( "+simbolo+","+ef.getName()+" )"
+            transicion= Transicion(ef,simbolo)
+            print("Transicion generada:",transicion)
+            self._edoInit.getTransiciones().append(transicion)
+            self._edosAFN = list()
+            self._edosAFN.append(self._edoInit)
+            self._edosAFN.append(ef)
+            self._edosAceptacion = list()
+            self._edosAceptacion.append(ef)
+        else:
+            self._edoInit = None
+            self._alfabeto = set()
+            self._edosAceptacion = set()
+            self._edosAFN = set()
 
     def enumAFN(self):
-        c=0
+        c = 0
         for i in self._edosAFN:
             print("Nombre anterior: "+i.getName())
             i.setName(str(c))
             print("Nombre nuevo: "+i.getName())
-            c+=1
+            c += 1
         return self._edosAFN
+
     def printTupla(self):
-        c=0
-        self._edosAFN=self.enumAFN()
-        tupla="Σ{ "
+        self._edosAFN = self.enumAFN()
+        tupla = "Σ{ "
         for i in self._alfabeto:
-            tupla+=i+" "
-        tupla+="}\nS={ "
+            tupla += i+" "
+        tupla += "}\nS={ "
         for i in self._edosAFN:
-            tupla+=i.getName()+" "
-        tupla+="}\nF{ "
+            tupla += i.getName()+" "
+        tupla += "}\nF{ "
         for i in self._edosAceptacion:
-            tupla+=i.getName()+" "
-        tupla+="}\nI{ "+self._edoInit.getName()+" }\nδ={\n   "
+            tupla += i.getName()+" "
+        tupla += "}\nI{ "+self._edoInit.getName()+" }\nδ={\n   "
         for i in self._edosAFN:
             for j in i.getTransiciones():
-                tupla+="("+i.getName()+")->"+j+"\n   "
-        tupla+="}\n"
+                tupla += "("+i.getName()+")->"+j.stringTransicion()+"\n   "
+        tupla += "}\n"
         return tupla
 
     def unirAFN(self, aut):
-        epsilon="ε"
-        e1=Estados()
-        e2=Estados()
-        e2.setAceptacion(True)
-        transicion="("+epsilon+","+self._edoInit.getName()+")"
+        epsilon = "ε"
+        e1 = Estados()
+        e2 = Estados()
+        #transicion = "("+epsilon+","+self._edoInit.getName()+")"
+        transicion=Transicion(self._edoInit,epsilon)
         e1.getTransiciones().append(transicion)
-        transicion="("+epsilon+","+aut._edoInit.getName()+")"
+        #transicion = "("+epsilon+","+aut._edoInit.getName()+")"
+        transicion=Transicion(aut._edoInit,epsilon)
         e1.getTransiciones().append(transicion)
         for i in self._edosAceptacion:
-            i.setAceptacion(False)
-            transicion="("+epsilon+","+e2.getName()+")"
+            #transicion = "("+epsilon+","+e2.getName()+")"
+            transicion=Transicion(e2,epsilon)
             i.getTransiciones().append(transicion)
-        for i in aut._edosAFN:
             i.setAceptacion(False)
-            transicion="("+epsilon+","+e2.getName()+")"
+        for i in aut._edosAceptacion:
+            #transicion = "("+epsilon+","+e2.getName()+")"
+            transicion=Transicion(e2,epsilon)
             i.getTransiciones().append(transicion)
-        self._alfabeto.union(aut._alfabeto)
+            i.setAceptacion(False)
+        e2.setAceptacion(True)
+        self._alfabeto.update(aut._alfabeto)
         self._edosAFN.append(e1)
         self._edosAFN.append(e2)
+        self._edosAFN.extend(aut._edosAFN)
         self._edosAceptacion.clear()
         self._edosAceptacion.append(e2)
-        self._edoInit=e1
+        self._edoInit = e1
         self.enumAFN()
         return self
 
 
-
-aut=Automata("A")
-aut2=Automata("B")
+aut = Automata("A")
+aut2 = Automata("B")
 print(aut.printTupla())
 print(aut2.printTupla())
-aut=aut.unirAFN(aut2)
+aut = aut.unirAFN(aut2)
 print(aut.printTupla())
---
+++ practica1Py/AFN/Transiciones.py
@@ -0,0 +1,38 @@
+from Estados import *
+
+class Transicion:
+    def __init__(self, estadoSig=None,simbolo=None):
+        if estadoSig is not None and simbolo is not None:
+            self._estadoSig=estadoSig
+            self._simbolo=simbolo
+        elif simbolo is not None and estadoSig is None:
+            self._estadoSig=None
+            self._simbolo=simbolo
+        elif estadoSig is not None and simbolo is None:
+            self._estadoSig=estadoSig
+            self._simbolo=None
+        else:
+            self._simbolo=None
+            self._estadoSig=None
+    
+    def getEstadoSig(self):
+        return self._estadoSig
+    
+    def setEstadoSig(self,estadoSig):
+        self._estadoSig=estadoSig
+    
+    def getSimbolo(self):
+        return self._simbolo
+    
+    def setSimbolo(self,simbolo):
+        self.simbolo=simbolo
+    
+    def printTransicion(self):
+        print("(",self._simbolo,'->',self._estadoSig.getToken(),")")
+    
+    def stringTransicion(self):
+        return '('+self._simbolo+'->'+self._estadoSig.getName()+')'
+    
+    def __str__(self):
+        return '('+self._simbolo+'->'+self._estadoSig.getName()+')'
+
