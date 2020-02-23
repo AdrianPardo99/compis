5785b7ece11fa51e502b4d6581255c8955aa696e
Adrian González Pardo <gozapaadr@gmail.com>
2020-02-23 17:16:56 -0600

    Modificacion de función concatenación para visualizar el alfabeto que maneja con pruebas sobre main.py

---
+++ practica1Py/main.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("AFN/")
+from Automata import *
+
+
+aut = Automata("A")
+aut2 = Automata("B")
+aut.printTupla()
+aut2.printTupla()
+aut = aut.concatenarAFN(aut2)
+aut.printTupla()
+aut3 = Automata("C")
+aut = aut.concatenarAFN(aut3)
+aut.printTupla()
+aut = aut.cerraduraPo()
+aut=aut.cerraduraOp()
+aut4=Automata("D")
+aut=aut.unirAFN(aut4)
+aut.printTupla()
+aut.AFNtoAFD()
+print(aut.printTupla())
